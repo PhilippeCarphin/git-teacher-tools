42bc0275a383406bfe0e9c013bed80243184c76b
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-11-08 03:08:56 -0500

    Niced up the code

--- inf1995-stuff/init-correction-inf1995.sh
+++ inf1995-stuff/init-correction-inf1995.sh
@@ -3,7 +3,14 @@ script_pwd=$(dirname $0)
 tp_date="2017-10-27 13:37"
 
 # Do a find command to note all *.o *.hex etc files
-find . -name '*.o' -o -name '*.a' -o -name '*.d' -o -name '*.hex' -o -name '*.out' -o -name '*.out.map' | tee fichiers_indesirables.lst
+names="-name '*.o' \
+-o -name '*.a' \
+-o -name '*.d' \
+-o -name '*.hex' \
+-o -name '*.out' \
+-o -name '*.out.map'"
+# Note: the eval here is important, it doesn't work without it.
+eval find . $names | tee fichiers_indesirables.lst
 
 # Maybe checout the last commit made before the due date
 # Look atgit checkout $(git rev-list -n 1 --before="2009-07-27 13:37" master)
@@ -17,5 +24,3 @@ git checkout -b correction_tp8
 team=$(basename $PWD | sed 's/.*inf1995-\([0-9]*\)$/\1/')
 $script_pwd/gen-inf1995-correction-file --correcter "Philippe Carphin" --team \
 	$team --section 01 --tp "TP8 Organisation de projet" > "Correction_TP8.txt"
-
-
