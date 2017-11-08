266a1585420af34bb9d79e5405d53b6cbf95527e
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-11-07 22:11:50 -0500

    TODO list for a script that will be run inside each repo after cloning

---
+++ init-correction.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Do a find command to note all *.o *.hex etc files
+
+# Maybe checout the last commit made before the due date
+
+# Create a branch,
+
+# Drop a correction file in the right place (use my gen-correction-file script
+# but modify it so that it takes only parameters and has the file content as a
+# string inside the gen-correction-script file itself.
+
+
