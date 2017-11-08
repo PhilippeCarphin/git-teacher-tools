1406d7f47384721960ba9b30cc7b6c9674823533
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-11-07 23:22:14 -0500

    Added branch creation and checkout

--- inf1995-stuff/init-correction-inf1995.sh
+++ inf1995-stuff/init-correction-inf1995.sh
@@ -2,10 +2,12 @@
 script_pwd=$(dirname $0)
 
 # Do a find command to note all *.o *.hex etc files
+find . -name '*.o' -o -name '*.a' -o -name '*.d' -o -name '*.hex' -o -name '*.out' -o -name '*.out.map' | tee fichiers_indesirables.lst
 
 # Maybe checout the last commit made before the due date
 
 # Create a branch,
+git checkout -b correction_tp8
 
 # Drop a correction file in the right place
 team=$(basename $PWD | sed 's/.*inf1995-\([0-9]*\)$/\1/')
