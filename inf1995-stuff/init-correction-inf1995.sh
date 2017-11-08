f97143c731453efb49c1f89e3853eddd99c3a2e3
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-11-07 22:58:12 -0500

    Removed un-needed echo

--- inf1995-stuff/init-correction-inf1995.sh
+++ inf1995-stuff/init-correction-inf1995.sh
@@ -9,7 +9,6 @@ script_pwd=$(dirname $0)
 
 # Drop a correction file in the right place
 team=$(basename $PWD | sed 's/.*inf1995-\([0-9]*\)$/\1/')
-echo $team
 $script_pwd/gen-inf1995-correction-file --correcter "Philippe Carphin" --team \
 	$team --section 01 --tp "TP8 Organisation de projet" > "Correction_TP8.txt"
 
