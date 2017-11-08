0a4de339549e57160268b7821aa6c4d4222cdf8e
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-11-07 21:50:26 -0500

    Ajout d'un filtre pour créer le fichier liste de repos en copy-pastant la page du site du cours

---
+++ inf1995-team-filter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+grep -v "[\t ]*#.*" |
+# Isolate the part of each line that is a string of consecutive digits
+# \1 <- \([0-9]*\)
+# and replace it by
+# githost.git.polymtl.ca/git/inf1995-\1
+sed 's/[^0-9]*\([0-9]*\)[^0-9]*/githost.gi.polymtl.ca\/git\/inf1995-\1/' |
+# remove duplicates
+sort | uniq
